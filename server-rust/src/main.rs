e5af6010507f0c4e2aa8948990a99b975f34aae2
metens <nathanmetens1@gmail.com>
2026-05-13 11:31:39 -0700

    Creating storage folder for files

--- server-rust/src/main.rs
+++ server-rust/src/main.rs
@@ -1,10 +1,18 @@
 use axum::{
-    routing::get,
+    routing::{get, post},
     Router,
 };
 
+use std::{fs};
+
 #[tokio::main]
 async fn main() {
+
+    const STORAGE_DIR: &str = "storage"; // all uploaded files will be stored in './storage'
+
+    fs::create_dir_all(STORAGE_DIR) // create the storage folder if it does not exist
+        .expect("failed to create storage directory"); // if failuer
+
     // build our application with a single route
     let app = Router::new().route("/", get(|| async { "Hello, World!" }));
 
