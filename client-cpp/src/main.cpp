af56e0152706d17ac51f94c70125e0b4e0f69d60
metens <nathanmetens1@gmail.com>
2026-05-13 16:49:27 -0700

    Cleaning up client folder, making cmake file

--- client-cpp/src/main.cpp
+++ client-cpp/src/main.cpp
@@ -1,8 +1,25 @@
 #include <iostream>
+#include <cstdlib>
+#include <string>
 
-int main() {
+int main(int argc, char* argv[]) {
 
-    std::cout << "Hello" << std::endl;
+    if (argc != 2) {
+        std::cout << "Usage: ./storagedrive_client <file_path>\n";
+        return 0;
+    }
 
-    return 0;
+    std::string file_path = argv[1];
+
+    std::string command = 
+        "curl -F \"file=@" + file_path + "\" http://127.0.0.1:3000/upload";
+
+    std::cout << "Uploading file: " << file_path << "\n";
+
+    int result = std::system(command.c_str());
+
+    if (result == 0) std::cout << "\nUpload complete.\n";
+    else std::cout << "\nUpload failed.\n";
+
+    return result;
 }
