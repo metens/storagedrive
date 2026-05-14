f68d102bbd417bc912d241ac308c98747f9b3aec
metens <nathanmetens1@gmail.com>
2026-05-14 00:13:47 -0700

    Creating http upload methods instead of curl for upload command into rust backend

--- client-cpp/src/main.cpp
+++ client-cpp/src/main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <cstdlib>
 #include <string>
-#include <cpr/cpr.h>
+#include <cpr/cpr.h> // docs: https://docs.libcpr.dev/advanced-usage.html#other-request-methods
 
 int test() {
     /*
@@ -21,14 +21,59 @@ int test() {
     return 0;
 }
 
+void upload_file(const std::string& filepath) {
+    cpr::Response r = 
+        cpr::Post(
+            cpr::Url{"http://127.0.0.1:3000/upload"},
+            cpr::Multipart{
+                {"file", cpr::File{filepath}}
+            }
+        );
+
+        std::cout << "Status Code: " << r.status_code << std::endl;
+        std::cout << r.text << std::endl;
+}
+
+void list_files() {
+    std::string url = "https://nathanmetens.dev";
+    
+    cpr::Response r = 
+        cpr::Get(cpr::Url{url},
+        cpr::Authentication{"user", "pass", cpr::AuthMode::BASIC},
+        cpr::Parameters{{"anon", "true"}, {"key", "value"}});
+
+    std::cout << r.status_code << std::endl;              // 200
+    std::cout << r.header["content-type"] << std::endl;   // application/json; charset=utf-8
+    std::cout << r.text << std::endl;                     // JSON text string
+    return;
+}
+
+void download_file(char* file) {
+
+}
+
 int main(int argc, char** argv) {
 
-    if (argc != 2) {
-        std::cout << "Usage: ./storagedrive_client <file_path>\n";
+    if (argc != 3) {
+        std::cout << "Usage:\n";
+        std::cout << "./storagedrive_client upload <file>\n";
+        std::cout << "./storagedrive_client list\n";
+        std::cout << "./storagedrive_client download <file>\n";
         return 0;
     }
 
-    std::string file_path = argv[1];
+    std::string command = argv[1];
+
+    if (command == "upload") 
+        upload_file(argv[2]);
+    else if (command == "list") 
+        list_files();
+    else if (command == "download") 
+        download_file(argv[2]);
+    else std::cout << "Unknown command." << std::endl;
+
+
+    /*std::string file_path = argv[1];
 
     std::string command = 
         "curl -F \"file=@" + file_path + "\" http://127.0.0.1:3000/upload";
@@ -40,7 +85,8 @@ int main(int argc, char** argv) {
     if (result == 0) std::cout << "\nUpload complete.\n";
     else std::cout << "\nUpload failed.\n";
 
-    test();
+    test();*/
 
-    return result;
+    //return result;
+    return 0;
 }
